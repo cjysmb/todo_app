2770d71cd4fba03dd973a3b32480a45042a9712f
cjsamboa <cjsamboa@gmail.com>
2022-08-29 00:52:59 +0800

    add changing of background

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,10 +1,38 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Navbar from '../custom_components/Navbar';
+import * as Bg from '../../static/backgrounds/index';
 
 const Main = ({children}) => {
 
+    const [value, setValue] = useState(0);
+ 
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setValue((v) => {
+                return v === 4 ? 0 : v + 1;
+            })
+        }, 20000);
+        return () => clearInterval(interval);
+    }, [])
+
+    const images = [
+        Bg.Road,
+        Bg.Seastars,
+        Bg.Mountain,
+        Bg.Stars,
+        Bg.Sunset
+    ]
+
+    const mainBg = {
+        backgroundImage: `url(${images[value]})`,
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+        backgroundPosition: 'center',
+        overflow: 'hidden',
+    }
+
     return(
-        <div className="main-bg">
+        <div className="main-bg" style={{...mainBg}}>
             <Navbar />
             {children}
         </div>
--- src/static/backgrounds/index.js
+++ src/static/backgrounds/index.js
@@ -1,5 +1,9 @@
 import Road from './asphalt.jpg';
+import Mountain from './mountain.jpg';
+import Seastars from './seastars.jpg';
+import Stars from './stars.jpg';
+import Sunset from './sunset.jpg';
 
 export {
-    Road
+    Road, Mountain, Seastars, Stars, Sunset
 }
